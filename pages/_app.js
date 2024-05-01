b657d2453f26d0303fb76c594dbd0639ab8ee592
Ben Do <bdo.appworkshop@gmail.com>
2024-05-01 17:14:03 -0500

    Wrapped Layout around Component.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import "@/styles/globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Layout from '../components/layout/layout';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
