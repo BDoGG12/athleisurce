4a7b9ea41640e550ee2586f8cc0f2201cec51835
Ben Do <bdo.appworkshop@gmail.com>
2024-05-09 16:30:03 -0500

    Wrapped mapped product in div, passed in key property value for div.

--- components/product-list/product-list.js
+++ components/product-list/product-list.js
@@ -8,15 +8,17 @@ const ProductList = ({ products }) => {
   return (
     <div className={classes.productList}>
       {products.map(product => (
-        <Link href={`/product-detail/${product.fields.name}`}>
-          <Card key={product.sys.id} style={{ width: '18rem' }}>
-            <Card.Img variant='top' src={product.fields.images[0].fields.file.url} />
-            <Card.Body>
-              <Card.Title>{product.fields.name}</Card.Title>
-              <Card.Text>{product.fields.price}</Card.Text>
-            </Card.Body>
-          </Card>
-        </Link>
+        <div key={product.sys.id}>
+          <Link href={`/product-detail/${product.fields.name}`}>
+            <Card style={{ width: '18rem' }}>
+              <Card.Img variant='top' src={product.fields.images[0].fields.file.url} />
+              <Card.Body>
+                <Card.Title>{product.fields.name}</Card.Title>
+                <Card.Text>{product.fields.price}</Card.Text>
+              </Card.Body>
+            </Card>
+          </Link>
+        </div>
       ))}
     </div>
   );
