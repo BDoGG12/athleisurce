072d28e979238e068b3dba97b355867d5717e71b
Ben Do <bdo.appworkshop@gmail.com>
2024-05-09 14:36:24 -0500

    Changed background color to light, added search icon button, inserted nav links to shirts and pants, removed collapsed Navbar. Added links for sign-up and log-in.

--- components/navigationBar/navigationBar.js
+++ components/navigationBar/navigationBar.js
@@ -2,26 +2,18 @@
 import Link from 'next/link';
 import classes from './navigationBar.module.css';
 import { Nav, Navbar, NavDropdown, Button, Form, Container } from 'react-bootstrap';
-import {FaShoppingCart} from 'react-icons/fa';
+import { FaShoppingCart, FaSearch } from 'react-icons/fa';
+import {createClient} from 'contentful';
 
-const NavigationBar = ({  }) => {
+const NavigationBar = ({ }) => {
+  const onPress = () => {
+    console.log('This event is not yet implemented');
+  }
   return (
     <>
-    <Navbar bg='dark' data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#">Athleisurce</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="/shirts">Shirts</Nav.Link>
-            <Nav.Link href="/pants">Pants</Nav.Link>
-            <Nav.Link href="/sign-up">Sign Up</Nav.Link>
-            <Nav.Link href="/log-in">Log In</Nav.Link>
-          </Nav>
+      <Navbar bg='light' data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#">Athleisurce</Navbar.Brand>
           <Form className="d-flex">
             <Form.Control
               type="search"
@@ -29,17 +21,22 @@ const NavigationBar = ({  }) => {
               className="me-2"
               aria-label="Search"
             />
-            <Button variant="outline-primary">Search</Button>
+            <Button onClick={onPress} variant='outline-primary'>
+              <FaSearch />
+            </Button>
           </Form>
+          <Nav.Link href="/shirts">Shirts</Nav.Link>
+          <Nav.Link href="/pants">Pants</Nav.Link>
           <Nav>
             {/* Cart Icon */}
             <Nav.Link href='/cart'>
               <FaShoppingCart size={20} />
             </Nav.Link>
           </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          <Nav.Link href="/sign-up">Sign Up</Nav.Link>
+          <Nav.Link href="/log-in">Log In</Nav.Link>
+        </Container>
+      </Navbar>
     </>
   );
 };
