7fceb040e88c0a32a9eb9b7771dce311824a9afb
Ben Do <bdo.appworkshop@gmail.com>
2024-05-09 14:38:34 -0500

    Added ProductDetailPage.

---
+++ pages/product-detail/[slug].js
@@ -0,0 +1,14 @@
+
+const ProductDetailPage = () => {
+
+  return (
+    <div>
+      <h1>Product Name</h1>
+      <p>Description</p>
+      <p>Price</p>
+      <button>Add to Cart</button>
+    </div>
+  );
+};
+
+export default ProductDetailPage;
