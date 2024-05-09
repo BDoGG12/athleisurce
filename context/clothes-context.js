e86b2e943b2737808b1a76f1a7f497248f05906f
Ben Do <bdo.appworkshop@gmail.com>
2024-05-09 14:34:38 -0500

    Implemented ClothesContext.

---
+++ context/clothes-context.js
@@ -0,0 +1,21 @@
+import {createContext, useState, useContext} from 'react';
+
+export const ClothesContext = createContext();
+
+export const ClothesProvider = ({children}) => {
+  const [shirts, setShirts] = useState();
+
+  const [pants, setPants] = useState();
+
+  const [searchResults, setSearchResults] = useState([]);
+
+  return (
+    <ClothesContext.Provider value={{shirts, setShirts, pants, setPants, searchResults, setSearchResults}}>
+      {children}
+    </ClothesContext.Provider>
+  );
+};
+
+export const useClothesContext = () => {
+  return useContext(ClothesContext);
+};
