745d7b1658a29011d46fbf6a19c8376f5bab1b36
Ben Do <bdo.appworkshop@gmail.com>
2024-05-09 14:38:17 -0500

    Added ClothesProvider context.

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../components/layout/layout';
+import { ClothesProvider } from '../context/clothes-context';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ClothesProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ClothesProvider>
     </>
   );
 }
