a96b51130b1be219995d119730b8fdd3f9026110
Ben Do <bdo.appworkshop@gmail.com>
2024-05-09 16:24:03 -0500

    Renamed to ShirtsPage. Imported createClient hook form contentful to fetch shirt products.

--- pages/shirts/index.js
+++ pages/shirts/index.js
@@ -1,12 +1,36 @@
+import {useRouter} from 'next/router';
+import {createClient} from 'contentful';
+import {useState} from 'react';
+import ProductList from '../../components/product-list/product-list';
 
+const client = createClient({
+  space: 'p843ovnyw7tf',
+  accessToken: 'L-nXGukJbr9-8cXH318T_7Ibn4-qm2sZIhmnXOdIvkU'
+});
 
-const ShirtsPage = () => {
+const ShirtsPage = ({products}) => {
+
+  const router = useRouter();
+  const { page } = router.query;
+  console.log('shirts', products);
 
   return (
-    <>
-    <h1>Shirts Page</h1>
-    </>
+    <div>
+      <h1>Shirts</h1>
+      <ProductList products={products} />
+    </div>
   );
 };
 
-export default ShirtsPage
+export async function getStaticProps() {
+  const { items } = await client.getEntries({ content_type: 'clothes' });
+  const filteredItems = items.filter(item => item.fields.category === 'shirts')
+
+  return {
+    props: {
+      products: filteredItems
+    }
+  };
+}
+
+export default ShirtsPage;
