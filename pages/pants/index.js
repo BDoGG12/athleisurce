e598deded5a0d544167e08d6c2a3bbda4ba4921a
Ben Do <bdo.appworkshop@gmail.com>
2024-05-09 16:31:19 -0500

    Passed in products prop for ProductList component, filtered for pants in getStaticProps method.

--- pages/pants/index.js
+++ pages/pants/index.js
@@ -1,12 +1,35 @@
+import {useRouter} from 'next/router';
+import {createClient} from 'contentful';
+import {useState} from 'react';
+import ProductList from '../../components/product-list/product-list';
 
+const client = createClient({
+  space: 'p843ovnyw7tf',
+  accessToken: 'L-nXGukJbr9-8cXH318T_7Ibn4-qm2sZIhmnXOdIvkU'
+});
 
-const PantsPage = () => {
+const PantsPage = ({products}) => {
+
+  const router = useRouter();
+  const { page } = router.query;
 
   return (
-    <>
-      <h1>Pants Page</h1>
-    </>
+    <div>
+      <h1>Pants</h1>
+      <ProductList products={products} />
+    </div>
   );
 };
+
+export async function getStaticProps() {
+  const { items } = await client.getEntries({ content_type: 'clothes' });
+  const filteredItems = items.filter(item => item.fields.category === 'pants')
+
+  return {
+    props: {
+      products: filteredItems
+    }
+  };
+}
 
 export default PantsPage;
