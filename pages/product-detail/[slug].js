815f9f2669debc1e0fe2281cec6801719dd3817c
Ben Do <bdo.appworkshop@gmail.com>
2024-05-17 10:43:21 -0500

    Imported useRouter, createClient, fetchAllProductSlugs, and ProductDetail component to provide product details of selected item.

--- pages/product-detail/[slug].js
+++ pages/product-detail/[slug].js
@@ -1,15 +1,50 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+import { createClient } from 'contentful';
+import { fetchAllProductSlugs } from '../../utils/contentful';
+import ProductDetail from '../../components/product-detail/product-detail';
 
-const ProductDetailPage = () => {
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+});
+
+const ProductDetailPage = ({ product }) => {
+  console.log('product detail', product);
+  const { name, description, price, images } = product.fields;
+  const { fields } = images[0];
+  const { file } = fields;
+  const { url } = file;
 
   return (
-    <div>
-      <h1>Product Name</h1>
-      <p>Description</p>
-      <p>Price</p>
-      <button>Add to Cart</button>
-    </div>
+    <ProductDetail name={name} description={description} price={price} url={url}  />
   );
+};
+
+export const getStaticPaths = async () => {
+  const slugs = await fetchAllProductSlugs();
+  const paths = slugs.map((slug) => ({
+    params: { slug },
+  }));
+
+  return {
+    paths,
+    fallback: false
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
+  console.log('slug', slug);
+
+  const { items } = await client.getEntries({ content_type: 'clothes' });
+  const foundItem = items.find(item => item.fields.name === slug);
+
+  return {
+    props: {
+      product: foundItem
+    }
+  }
 };
 
 export default ProductDetailPage;
