8667d4164101f942b7c8f6bf428e9d753cac8f7e
Ben Do <bdo.appworkshop@gmail.com>
2024-05-20 07:30:22 -0500

    Added ATHLEISURCE_BASE_URL to configuration.

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    ATHLEISURCE_BASE_URL: process.env.ATHLEISURCE_BASE_URL
   }
 };
 
