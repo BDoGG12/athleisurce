5b0b212c52aea7d56282755a7b6853bccd87e689
Ben Do <bdo.appworkshop@gmail.com>
2024-05-24 22:49:24 -0500

    Imported useEffect, useState, axios, and useClothesContext. Performed post request to api for Cart.

--- components/product-detail/product-detail.js
+++ components/product-detail/product-detail.js
@@ -1,13 +1,72 @@
 import { Button } from 'react-bootstrap';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from './product-detail.module.css';
+import axios from 'axios';
+import { useClothesContext } from "../../context/clothes-context";
 
-const ProductDetail = ({ name, description, price, url }) => {
+const ProductDetail = ({ title, description, price, url }) => {
   const descriptionArray = description.split('\n').map(line => line.replace(/^\s*-\s*/, ''));
+
+  const {logInUser, sessions} = useClothesContext();
+  console.log('user', logInUser);
+  const [userInfo, setUserInfo] = useState();
+  const {name} = logInUser;
+  let names;
+  if (name) {
+    names = name.split(' ');
+  }
+
+  useEffect(() => {
+    const {id} = logInUser;
+    axios.get(`https://localhost:44375/api/GetCustsomerById/${id}`)
+    .then((res) => {
+      setUserInfo(res.data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+    
+  }, [logInUser]);
+
+  const addToCartHandler = () => {
+    const productInfo = {
+      title: title,
+      description: description,
+      price: price,
+      size: 'small',
+      imageUrl: url,
+      category: 'clothes', 
+    }
+    console.log('user info', userInfo);
+    axios.post(`https://localhost:44375/api/Cart`, {
+      quantity: 1,
+      customerInfo: {
+        firstName: names[0],
+        lastName: names[1],
+        emailAddress: 'email@test.com',
+        phoneNumber: '1234567890',
+        password: 'testing',
+        address: {
+          streetName: 'street',
+          city: 'city',
+          state: 'state',
+          postalCode: '60061',
+          country: 'US'
+        }
+      },
+      productInfo: productInfo
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  };
 
   return (
     <div>
-      <h1>{name}</h1>
+      <h1>{title}</h1>
       <img className={classes.image} src={url} alt={name} />
       <ul>
         {descriptionArray.map((item, index) => (
@@ -15,7 +74,7 @@ const ProductDetail = ({ name, description, price, url }) => {
         ))}
       </ul>
       <p>Price: ${price}</p>
-      <Button>Add to Cart</Button>
+      <Button onClick={addToCartHandler}>Add to Cart</Button>
     </div>
   );
 };
