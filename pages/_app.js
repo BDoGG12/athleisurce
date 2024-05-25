0a4791711aa8f1ebcf237efd4518dc063f0d9aad
Ben Do <bdo.appworkshop@gmail.com>
2024-05-24 23:51:39 -0500

    Imported SessionProvider.

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from '../components/layout/layout';
 import { ClothesProvider } from '../context/clothes-context';
 import React from 'react';
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <ClothesProvider>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </ClothesProvider>
-    </>
+    </SessionProvider>
   );
 }
