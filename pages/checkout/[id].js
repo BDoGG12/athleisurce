3b8263f1d4245338fee10c289e80aaf3f1a420f1
Ben Do <bdo.appworkshop@gmail.com>
2024-06-05 08:58:23 -0500

    Added CheckoutPage and OrderPage.

---
+++ pages/checkout/[id].js
@@ -0,0 +1,13 @@
+import React from "react";
+import Checkout from "../../components/checkout/checkout";
+
+const CheckoutPage = () => {
+
+    return (
+        <>
+        <Checkout />
+        </>
+    )
+};
+
+export default CheckoutPage;
